008f9314885e2e69b190eefbf46eb78fe02de0ff
Mona04 <dh0450@endcs.net>
2023-07-28 18:32:30 +0900

    asdf

--- src/app/_components/masthead.tsx
+++ src/app/_components/masthead.tsx
@@ -1,11 +1,16 @@
+'use client'
 import style from "@/styles/components/masthead.module.scss"
 import {BLOG_NAME} from "@/lib/config/blog-config"
 
 function MastHead() {
-
+  const onClickMenu = () => {
+    
+    // Add your custom logic here to handle the button click event.
+  };
   return (
     <div className={style.masthead}>
-      <div className={style['menu']} title="menu">
+      <div className={style['menu-btn']} title="menu"
+           onClick={onClickMenu}>
         <div className={style["menu-lines"]}>
           <div className={style["line"]}></div>
           <div className={style["line"]}></div>
