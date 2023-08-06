91a9cb0a2b36d2b65a20e33cb136685c834c1b39
Mona04 <moksha1905@gmail.com>
2023-08-07 01:26:53 +0900

    asfd

--- src/app/blog/[...categories]/page.tsx
+++ src/app/blog/[...categories]/page.tsx
@@ -1,21 +1,45 @@
 import { allPosts } from '@/contentlayer/generated'
 import PostView from './post-view'
+import CategoryView from './category-view'
+import {blogSlugs, blogSlugDict} from 'utils/blog-helper'
 
+type Params = {
+  params: { categories: string[] } 
+}
 
-// slug 에서 blog 는 제외한 나머지가 필요함
-export const generateStaticParams = async () => allPosts.map((post) => ({ categories: post._raw.flattenedPath.split('/').slice(1) }))
-
-export const generateMetadata = ({ params }: { params: { categories: string[] } }) => {
+const getPost = (params: { categories: string[] }  ) => {
   const cur = ['blog', ...params.categories].join('/')
-  const post = allPosts.find((post) => post._raw.flattenedPath === cur)
-  if (!post) throw new Error(`Post not found for slug: params : ${cur}`)
+  return allPosts.find((post) => post._raw.flattenedPath === cur)
+}
+
+export const generateStaticParams = () => blogSlugs;
+
+export const generateMetadata = ({ params }: Params) => {
+  const post = getPost(params);
+  if (!post) {
+    return {}
+    
+  }
   return { title: post.title }
 }
 
-export default function Page({ params }: { params: { categories: string[], name: string} }) {
-  const cur = ['blog', ...params.categories].join('/')
-  const post = allPosts.find((post) => post._raw.flattenedPath === cur)
-  if (!post) throw new Error(`Post not found for slug: ${params.categories}`)
+export default function Page({ params }: Params) {
+  const slug = ["", 'blog', ...params.categories].join('/');
+  if(slug in blogSlugDict == false) {
+    throw new Error(`Post not found for slug: ${params.categories}`)
+  }
+
+  if(blogSlugDict[slug].bPost == false)
+  {
+    return (
+      <CategoryView categories={slug}/>
+    )
+  }
+  const post = getPost(params);
+  if (!post){
+    console.log("asfd")
+  } 
+
 
   return (
     <div>
