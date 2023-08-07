5d85c6c7261c54091d993c0431d413d3571316ca
Mona04 <moksha1905@gmail.com>
2023-08-08 01:42:04 +0900

    asdf

--- jest.config.js
+++ jest.config.js
@@ -8,10 +8,19 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const config = {
-  setupFilesAfterEnv: ['<rootDir>/test/jest.setup.js'],
-  moduleDirectories: ['node_modules', '<rootDir>/test/', './src/'],
-  testEnvironment: 'jest-environment-jsdom',
-  //preset: "ts-jest"
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', './src/'],
+  testEnvironment: 'node',
+  
+  transformIgnorePatterns: [
+    "node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)",
+    "node_modules/contentlayer/dist"
+  ],
+  moduleNameMapper: {
+    // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
+    //"contentlayer": require.resolve('contentlayer'),
+  },
+  preset: "ts-jest"
 }
 
 module.exports = createJestConfig(config)
