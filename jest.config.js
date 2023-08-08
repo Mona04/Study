5819e4f42dcfb143882ade484548fe295578b09b
Mona04 <dh0450@endcs.net>
2023-08-08 18:30:12 +0900

    asdf

--- jest.config.js
+++ jest.config.js
@@ -11,16 +11,23 @@ const config = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleDirectories: ['node_modules', './src/'],
   testEnvironment: 'node',
-  
+  preset: "ts-jest",
   transformIgnorePatterns: [
-    "node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)",
-    "node_modules/contentlayer/dist"
+    'node_modules/(?!(contentlayer))', 
+
   ],
+  transform: {
+    "\\.[jt]sx?$": "babel-jest",
+    '.contentlayer\\.+\\.js?$': 'ts-jest',
+    'node_modules/contentlayer': 'ts-jest',
+    'C:\\Users\\user\\Desktop\\Study\\blogs\\study-log\\node_modules\\contentlayer\\dist\\client\\index.js': "babel-jest"
+  },
+
   moduleNameMapper: {
     // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
     //"contentlayer": require.resolve('contentlayer'),
-  },
-  preset: "ts-jest"
+   
+  }
 }
 
 module.exports = createJestConfig(config)
