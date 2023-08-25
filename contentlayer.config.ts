1d550de07638a8dc345f4f951ffe50200fa84dc4
Mona04 <moksha1905@gmail.com>
2023-08-26 01:05:03 +0900

    asdf

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,41 +1,21 @@
 // contentlayer.config.ts
 import { defineDocumentType, makeSource } from 'contentlayer/source-files'
-import highlight from 'rehype-prism-plus'
-import title from 'rehype-code-titles'
-import prettyCode from 'rehype-pretty-code'
 
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
-  filePathPattern: `**/*.md`,
-  contentType: 'markdown',
+  filePathPattern: `**/*.(md|mdx)`,
+  contentType: 'mdx',
   fields: {
-    title: { type: 'string', required: true },
-    date: { type: 'date', required: true },
-  },
-  computedFields: {
-    url: { type: 'string', resolve: (post) => `/posts/${post._raw.flattenedPath}` },
+    title:        { type: 'string',   required: true },
+    date:         { type: 'date',     required: true },
+    description:  { type: 'string',   required: false},
+    excerpt:      { type: 'string',   required: false},
   },
 }))
-//https://yiyb-blog.vercel.app/posts/nextjs-contentlayer-blog
+
 
 export default makeSource({
     contentDirPath: '_content', 
     documentTypes: [Post],
-    markdown:{ 
-      rehypePlugins: [
-   
-        [
-          prettyCode,
-          {
-            showLineNumbers: true,
-            grid: true,
-            theme: 'rose-pine-moon',
-            dark: 'github-dark-dimmed',
-            light: 'github-light',
-          }
-        ]
-      ]    
-    },
-    mdx:{ rehypePlugins: [highlight] }
 })
