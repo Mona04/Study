8b595928f5be2be690621d6e899aeaa7dc90b420
Mona04 <dh0450@endcs.net>
2023-09-04 18:17:22 +0900

    ㅁㄴㅇㄹ

---
+++ src/app/_components/code-highlight/code-highlight.test.tsx
@@ -0,0 +1,43 @@
+import {render, fireEvent, waitFor, act } from '@testing-library/react'
+
+import {TitleBar} from './code-highlight'
+
+describe('Code Highlight Test', () => {
+  it('Copy Button Test', async () => {
+    // navigator mock
+    Object.assign(navigator, {
+      clipboard: {
+        writeText : jest.fn(),
+      },
+    });
+
+    const code = `
+      #include <iostream>
+      void main()
+      {
+        std::cout << "hello world" << std::endl
+      }
+    `;
+    const props = {
+      properties: {
+        'data-language': 'c++',
+        'data-theme': 'default'
+      }
+    }
+
+    const screen = render(
+      <>
+        <TitleBar code={code} properties={props}/>
+      </>
+    );
+
+    const togglebtn = screen.getByRole('button',);
+    expect(togglebtn).toBeDefined();
+        
+    // 내부에서 async 로 state 바꿔서 not wrapped in act warning 뜨기 때문에 
+    // component update 를 기다리는 waitfor 필요
+    await waitFor(()=>fireEvent.click(togglebtn));
+
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith(code);
+  })
+})
